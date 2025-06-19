69c3c2182b4ac7f98bb781bb584356be0ed598bc
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-06-19 11:02:02 +0700

    Setting condition in models

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,10 +1,7 @@
 class Quest < ApplicationRecord
   validates :name, presence: true
-  
   before_save :trim_whitespace
-  
   private
-  
   def trim_whitespace
     self.name = name.strip if name.present?
   end
