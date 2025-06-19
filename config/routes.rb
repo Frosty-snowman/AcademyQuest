0cf832086d95231f5e35868497583725ec6b6d1f
Nattapat Pinrat <nattapatpinrat@Nattapats-MacBook-Air-2.local>
2025-06-19 13:08:27 +0700

    Set routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   root "quests#index"
 
   get "up" => "rails/health#show", as: :rails_health_check
-
+  resources :quests do
+    delete :destroy_multiple, on: :collection
+  end
+  resources :quests do
+    member do
+      patch :toggle_complete
+    end
+  end
 end
